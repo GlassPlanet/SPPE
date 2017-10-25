ac266089d8b97fbfeb3aeee6add84930b2c718cc
GlassPlanet <jeffryts12@gmail.com>
2017-10-25 13:14:38 -0400

    Added The UE4 Files
    This is mainly just a test.

---
+++ Source/SPPE/SPPEGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SPPEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPPE_API ASPPEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
